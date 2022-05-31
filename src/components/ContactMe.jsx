64cc1d201d239dd44dcecc50d994cace0c7bc4f7
IgnacioCalvagna <ignaciocalvagna@gmail.com>
2022-05-30 21:50:40 -0300

    finalizando adaptacion movil en contactame

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -4,65 +4,68 @@ import "../assets/css/contactMe.css";
 import Info from "./Info";
 import DondeEstamos from "./DondeEstamos";
 import mp from "../assets/img/mp.png";
-import pf from '../assets/img/pf.png'
-import rp from '../assets/img/rp.png'
+import pf from "../assets/img/pf.png";
+import rp from "../assets/img/rp.png";
 const ContactMe = () => {
   return (
     <>
-    <div className="container">
-
-      <div className="info">
-        <p className="mip">
-          Nuestros productos y en especial los aceites cannábicos tienen la más
-          alta <br />
-          concentración en su ADN de CBD. También, contamos con la más grande
-          variedad <br />
-          de cepas y combinaciones que atienen cada necesidad de manera
-          personalizada. <br />
-          <strong className="mip">¡No Dudes en consultarnos!</strong>
-        </p>
-      </div>
-
-      <div className="contact ">
-
-        <div className="divInfo">
-          <Info />
+      <div className="container">
+        <div className="info">
+          <p className="mip">
+            Nuestros productos y en especial los aceites cannábicos tienen la
+            más alta <br />
+            concentración en su ADN de CBD. También, contamos con la más grande
+            variedad <br />
+            de cepas y combinaciones que atienen cada necesidad de manera
+            personalizada. <br />
+            <strong className="mip">¡No Dudes en consultarnos!</strong>
+          </p>
         </div>
 
-        <div className="divHorario">
-          <h4>Horario de atencion</h4>
-          <strong> Lunes a Viernes: </strong> <br /> 10 - 13hs 15 - 20hs.
-          <br />
-          <strong>Sabados: </strong> <br /> 11 - 15hs.
-        </div>
+        <div className="contact container ">
+          <div className="divInfo">
+            <Info />
+          </div>
 
-        <div className="divMedioPago">
-          <h4 className="h4Medios">Medios de pago</h4>
-          <div className="seleccionPagos">
-            <div className="mediopago">
-              <img src={mp} width="20%" alt="" />
-              <img src={rp} width="20%" alt="" />
-              <img src={pf} width="20%" alt="" />
-              <img src={mp} width="20%" alt="" />
-              <img src={mp} width="20%" alt="" />
+          <div className="divHorario  ">
+            <h3 className="verde miH3">Horario de atencion</h3>
+
+            <div className="todoHora ">
+              <div className="semana ">
+                <strong> Lunes a Viernes: </strong> <br /> 10 - 13hs <br /> 15 -
+                20hs.
+              </div>
+              <div className="finde ">
+                <strong>Sabados: </strong> <br /> 11 - 15hs.
+              </div>
+            </div>
+            
+          </div>
+
+          <div className="divMedioPago">
+            <h4 className="h4Medios ">Medios de pago</h4>
+            <div className="seleccionPagos">
+              <div className="mediopago">
+                <img src={mp} width="20%" alt="" />
+                <img src={rp} width="20%" alt="" />
+                <img src={pf} width="20%" alt="" />
+                <img src={mp} width="20%" alt="" />
+                <img src={mp} width="20%" alt="" />
+              </div>
             </div>
           </div>
         </div>
 
-      </div>
-
-
-
-      <div className=" deAbajo">
-        <div className=" izquierda">
-          <Form />
-        </div>
-        <div className="derecha">
-          <DondeEstamos />
+        <div className=" deAbajo">
+          <div className=" izquierda">
+            <Form />
+          </div>
+          <div className="derecha">
+            <DondeEstamos />
+          </div>
         </div>
       </div>
-    </div>
-      </>
+    </>
   );
 };
 
--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,29 +1,34 @@
 import React from "react";
-import '../assets/css/info.css'
+import "../assets/css/info.css";
 const Info = () => {
   return (
     <>
       <div className="container totInfo">
-          <h3 className="infoContacto">Informacion contacto</h3>
-          <div className="basicInfo">
-                <p><strong>Email</strong></p>
-                <p>s.eric.y@gmail.com</p>
-                <p><strong>Telefono</strong></p>
-                <p>+54 9 01162119283</p>
+        <div className="basicInfo">
+          <h2 className="infoContacto">Informacion contacto</h2>
+          <div className="infoLine">
+            <div className="infoLineTitle">
+              <p> <strong> Email </strong></p> <p className="dato">s.eric.y@gmail.com</p>
+            </div>
           </div>
-          <div className="row">
-              <p><strong>Redes sociales</strong> </p>
-            <div className="col-3">
-                red1
+
+          <div className="infoLine">
+            <div className="infoLineTitle">
+              <p> <strong> Telefono </strong></p> <p className="numContact">+54 9 01162119283</p>
             </div>
-            <div className="col-3">
-                red2
-            </div>
-            <div className="col-3">
-                red3
-            </div>
-            
           </div>
+         
+        </div>
+        <div className="red">
+          <p className="misRedes">
+            <strong>Redes sociales</strong>{" "}
+          </p>
+          <div className="iconosRedes">
+            <div className="icon">red1</div>
+            <div className="icon">red2</div>
+            <div className="icon">red3</div>
+          </div>
+        </div>
       </div>
     </>
   );
